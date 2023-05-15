363dc0edae3babe893f5acf2c8623d68c8f4e11b
KID <50666207+KID421@users.noreply.github.com>
2023-05-15 10:43:43 +0800

    Update 發布 VR 版本

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -14,29 +14,53 @@ namespace KID
         private float bulletSpeed;
         [SerializeField, Header("生成子彈位置")]
         private Transform bulletSpawnPoint;
+        [Header("檢測 VR 頭盔蹲下區域")]
+        [SerializeField] private Vector3 boxCheckVRHeadSize;
+        [SerializeField] private Vector3 boxCheckVRHeadOffset;
+        [SerializeField, Header("要偵測的圖層")] private LayerMask layerHead;
 
         /// <summary>
         /// 蹲下 Y 位置
         /// </summary>
         private float crouchPosition = 0;
         /// <summary>
-        /// 蹲下 Y 位置
+        /// 原始 Y 位置
         /// </summary>
         private float originalPosition = 1;
 
         private bool isCrouch;
+
+        private bool hasWeapon;
         #endregion
 
         #region 事件
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1, 0.3f, 0.3f, 0.5f);
+            Gizmos.DrawCube(
+                transform.position + transform.TransformDirection(boxCheckVRHeadOffset),
+                boxCheckVRHeadSize);
+        }
+
         private void Update()
         {
             // PC 版本
             if (Input.GetKeyDown(KeyCode.Mouse0) && !isCrouch) Fire();
             if (Input.GetKeyDown(KeyCode.LeftControl)) Crouch();
+
+            CheckVRHeadInCrouchArea();
         }
         #endregion
 
         #region 方法
+        /// <summary>
+        /// 擁有武器
+        /// </summary>
+        public void HasWeapon()
+        {
+            hasWeapon = true;
+        }
+
         /// <summary>
         /// 發射子彈
         /// </summary>
@@ -57,6 +81,22 @@ namespace KID
             isCrouch = !isCrouch;
             float positionY = isCrouch ? crouchPosition : originalPosition;
             transform.position = new Vector3(0, positionY, -5);
+        }
+
+        private void CheckVRHeadInCrouchArea()
+        {
+            Collider[] hits = Physics.OverlapBox(
+                transform.position + transform.TransformDirection(boxCheckVRHeadOffset),
+                boxCheckVRHeadSize / 2, Quaternion.identity, layerHead);
+
+            if (hits.Length > 0)
+            {
+                isCrouch = true;
+            }
+            else
+            {
+                isCrouch = false;
+            }
         }
         #endregion
     }
